65b54127f40037acdea6f33d0460a9b152472a5b
yeongbhinkim <abc282v@naver.com>
2024-05-02 20:26:37 +0900

    first commit

---
+++ src/test/java/com/googoo/festivaldotcom/FestivalDotComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googoo.festivaldotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalDotComApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
