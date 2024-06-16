36be7d569832c9eb642227706026027048cb5c58
khm <zqxwcerv@gmail.com>
2024-06-17 00:26:14 +0900

    추가 커밋

--- src/main/java/com/googoo/festivaldotcom/function/login/presentation/LoginController.java
+++ src/main/java/com/googoo/festivaldotcom/function/login/presentation/LoginController.java
@@ -6,15 +6,23 @@ import com.googoo.festivaldotcom.global.utils.DetermineUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
+
+    private final OAuthService oAuthService;
+    private final UserRepository userRepository;
+
 
     /**
      *  홈 화면
@@ -24,7 +32,7 @@ public class LoginController {
     @Trace
     @RequestMapping("/")
     public String home(HttpServletRequest request) {
-        log.info("info={}", "home()호출됨");
+        log.info("home = {}", "home()호출됨");
 
         String view = DetermineUtil.determineView(request, "login/beforeLogin", "login/afterLogin");
         return view;
@@ -37,8 +45,29 @@ public class LoginController {
     @Trace
     @RequestMapping("/oauthlogin")
     public String login() {
-        log.info("info={}", "oauthlogin()호출됨");
+        log.info("oauthlogin = {}", "oauthlogin()호출됨");
         return "/login/loginForm";
+    }
+
+    /**
+     * 로그아웃 화면
+     * @return
+     */
+    @Trace
+    @GetMapping("/oauthlogOut")
+    public ResponseEntity<String> logOut(
+            @AuthenticationPrincipal JwtAuthentication user
+    ) {
+        log.info("oauthlogOut = {}", "oauthlogOut()호출됨");
+
+        Optional<User> userData = userRepository.selectId(user.id());
+
+        String Provider = userData.get().getProvider();
+
+        // OAuth 로그아웃 URL 가져오기
+        String logoutUrl =oAuthService.revokeToken(Provider, user.accessToken());
+
+        return ResponseEntity.ok(logoutUrl);
     }
 
 
