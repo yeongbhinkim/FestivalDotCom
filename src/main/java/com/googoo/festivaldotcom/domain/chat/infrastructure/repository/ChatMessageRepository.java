9dd0fd305ea144fb4beb755fbeefb3ea418adbb2
khm <zqxwcerv@gmail.com>
2024-07-10 00:04:24 +0900

    mongo repository 추가

---
+++ src/main/java/com/googoo/festivaldotcom/domain/chat/infrastructure/repository/ChatMessageRepository.java
@@ -0,0 +1,14 @@
+package com.googoo.festivaldotcom.domain.chat.infrastructure.repository;
+
+import com.googoo.festivaldotcom.domain.chat.domain.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    List<ChatMessage> findByRoomIdOrderBySentAtAsc(String roomId);
+
+}
