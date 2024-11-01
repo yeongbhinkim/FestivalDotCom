72c07cabaed536d130508e75ddd2caad4fca57e8
yeongbhinkim <abc282v@naver.com>
2024-11-01 13:47:48 +0900

    test6

--- src/main/java/com/googoo/festivaldotcom/FestivalDotComApplication.java
+++ src/main/java/com/googoo/festivaldotcom/FestivalDotComApplication.java
@@ -17,7 +17,7 @@ public class FestivalDotComApplication {
         // .env 파일에서 환경 변수 로드
 //        Dotenv dotenv = Dotenv.load();
         Dotenv dotenv = Dotenv.configure()
-                .directory("/app")  // .env 파일이 위치한 디렉토리 지정
+                .directory("/home/dotcom/config/")  // .env 파일이 위치한 디렉토리 지정
                 .load();
 
         String logFilePath = dotenv.get("LOG_FILE_PATH");
